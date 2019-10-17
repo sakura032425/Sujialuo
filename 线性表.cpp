8414a15ce6f63c75a4290880bf01d4efc3730f98
杨明昊 <1334223270@qq,com>
2019-10-17 14:56:01 +0800

    Update 线性表.cpp

--- 线性表.cpp
+++ 线性表.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
 #define LIST_INIT_SIZE          100
 #define LISTINCEREMENT          10
 typedef int ElemType;
@@ -11,6 +15,7 @@ typedef struct {
 } SqList;
 int main()
 {
+	srand((unsigned)time(NULL));
 	SqList L, La, Lb, Lc;
 	int InitList(SqList & L);
 	void DestroyList(SqList & L);
@@ -26,10 +31,141 @@ int main()
 	void ListDelete(SqList & L, int i, ElemType & e);
 	void visit(ElemType & e);
 	void ListTraverse(SqList L);
-	void Intersection(SqList La, SqList Lb, SqList Lc);
-	void Union(SqList La, SqList Lb);
+	void Intersection(SqList La, SqList Lb, SqList & Lc);
+	void Union(SqList La, SqList & Lb);
 	void Difference(SqList & La, SqList Lb);
-
+	InitList(La);
+	InitList(Lb);
+	InitList(Lc);
+	for (int i = 1; i <= 5; i++)
+		ListInsert(La, i, i);
+	for (int i = 3; i <= 7; i++)
+		ListInsert(Lb, i - 2, i);
+	while (1) {
+		cout << "***************************************************" << endl;
+		cout << "*              线性表操作                         *" << endl;
+		cout << "*              1 创建                             *" << endl;
+		cout << "*              2 销毁                             *" << endl;
+		cout << "*              3 清空                             *" << endl;
+		cout << "*              4 判断是否为空                     *" << endl;
+		cout << "*              5 判断长度                         *" << endl;
+		cout << "*              6 输出位置数值                     *" << endl;
+		cout << "*              7 定位                             *" << endl;
+		cout << "*              8 获得前驱                         *" << endl;
+		cout << "*              9 获取后继                         *" << endl;
+		cout << "*              10 插入数据                        *" << endl;
+		cout << "*              11 删除数据                        *" << endl;
+		cout << "*              12 对每个数据进行操作              *" << endl;
+		cout << "*              13 交集                            *" << endl;
+		cout << "*              14 并集                            *" << endl;
+		cout << "*              15 差集                            *" << endl;
+		cout << "请选择一个功能" << endl;
+		int n;
+		cin >> n;
+		switch (n) {
+		case 1: {
+			InitList(L);
+			break;
+		}
+		case 2: {
+			DestroyList(L);
+			break;
+		}
+		case 3: {
+			ClearList(L);
+			break;
+		}
+		case 4: {
+			if (ListEmpty(L))
+				cout << "不为空" << endl;
+			else
+				cout << "L为空表" << endl;
+			break;
+		}
+		case 5: {
+			cout << "表的长度为:" << Listlength(L) << endl;
+			break;
+		}
+		case 6: {
+			int i, e;
+			cout << "请输入数据的位置:";
+			cin >> i;
+			GetElem(L, i, e);
+			cout << e << endl;
+			break;
+		}
+		case 7: {
+			int e;
+			cout << "请输入要查找的数据:";
+			cin >> e;
+			cout << LocateElem(L, e) << endl;
+			break;
+		}
+		case 8: {
+			int cur_e, pre_e;
+			cout << "请输入数据:";
+			cin >> cur_e;
+			PriorElem(L, cur_e, pre_e);
+			cout << pre_e << endl;
+			break;
+		}
+		case 9: {
+			int cur_e, next_e;
+			cout << "请输入数据:";
+			cin >> cur_e;
+			NextElem(L, cur_e, next_e);
+			cout << next_e << endl;
+			break;
+		}
+		case 10: {
+			int i, e;
+			cout << "请输入要插入数据的位置:";
+			cin >> i;
+			cout << "请输入要插入的数据:";
+			cin >> e;
+			ListInsert(L, i, e);
+			break;
+		}
+		case 11: {
+			int i, e;
+			cout << "请输入要删除数据的位置:";
+			cin >> i;
+			ListDelete(L, i, e);
+			cout << "被删除的数据为" << e << endl;
+			break;
+		}
+		case 12: {
+			ListTraverse(L);
+			break;
+		}
+		case 13: {
+			Intersection(La, Lb, Lc);
+			for (int i = 0; i < Lc.length; i++)
+				cout << *Lc.elem + i << " ";
+			cout << endl;
+			break;
+		}
+		case 14: {
+			Union(La, Lb);
+			for (int i = 0; i < Lb.length; i++)
+				cout << *(Lb.elem + i) << " ";
+			break;
+		}
+		case 15: {
+			Difference(La, Lb);
+			for (int i = 0; i < La.length; i++)
+				cout << *(La.elem + i) << " ";
+			break;
+		}
+		default: {
+			cout << "退出程序" << endl;
+			return 0;
+			break;
+		}
+		}
+		system("pause");
+		system("cls");
+	}
 	return 0;
 }
 
@@ -78,7 +214,7 @@ int GetElem(SqList L, int i, ElemType& e)
 	if ((i > L.length) || (i < 1))
 		return 0;
 
-	e = *L.elem + i - 1;
+	e = *(L.elem + i - 1);
 	return 1;
 }
 
@@ -106,13 +242,13 @@ void PriorElem(SqList L, ElemType cur_e, ElemType& pre_e)
 {
 	int i = 0;
 
-	for ( ; i < L.length; i++)
-		if (*L.elem + i == cur_e)
+	for (; i < L.length; i++)
+		if (*(L.elem + i) == cur_e)
 			break;
-	if (i <= 1)
+	if (i < 1)
 		return;
 
-	pre_e = *L.elem + i - 1;
+	pre_e = *(L.elem + i - 1);
 }
 
 
@@ -120,29 +256,28 @@ void NextElem(SqList L, ElemType cur_e, ElemType& next_e)
 {
 	int i = 0;
 
-	for ( ; i < L.length; i++)
-		if (*L.elem + i == cur_e)
+	for (; i < L.length; i++)
+		if (*(L.elem + i) == cur_e)
 			break;
 	if (i == L.length - 1)
 		return;
 
-	next_e = *L.elem + i - 1;
+	next_e = *(L.elem + i + 1);
 }
 
 
 void ListInsert(SqList& L, int i, ElemType e)
 {
-	if ((i < 1) || (i > L.length))
+	if ((i < 1) || (i > L.length + 1))
 		return;
-
-	if (L.length >= L.listsize) {
+	if (L.length > L.listsize) {
 		L.elem = (ElemType *)realloc(L.elem, (L.listsize + 10) * sizeof(ElemType));
 		L.listsize += LISTINCEREMENT;
 	}
 	int k = L.length - 1;
-	for ( ; k >= i - 1; k--)
+	for (; k >= i - 1; k--)
 		*(L.elem + k + 1) = *(L.elem + k);
-	*(L.elem + k) = e;
+	*(L.elem + i - 1) = e;
 	L.length++;
 }
 
@@ -153,7 +288,7 @@ void ListDelete(SqList& L, int i, ElemType& e)
 		return;
 
 	e = *(L.elem + i - 1);
-	for ( ; i <= L.length; i++)
+	for (; i <= L.length; i++)
 		*(L.elem + i - 1) = *(L.elem + i);
 	L.length--;
 }
@@ -175,9 +310,10 @@ void Intersection(SqList La, SqList Lb, SqList& Lc)
 {
 	int k = 0;
 
+	ClearList(Lc);
 	for (int i = 0; i < La.length; i++) {
 		for (int j = 0; j < Lb.length; j++) {
-			if (La.length == Lb.length) {
+			if (*(La.elem + i) == *(Lb.elem + j)) {
 				*(Lc.elem + k) = *(La.elem + i);
 				k++;
 				Lc.length++;
@@ -187,36 +323,37 @@ void Intersection(SqList La, SqList Lb, SqList& Lc)
 }
 
 
-void Union(SqList& La, SqList Lb)
+void Union(SqList La, SqList& Lb)
 {
 	for (int i = 0; i < La.length; i++) {
 		int k = 1;
 		for (int j = 0; j < Lb.length; j++) {
-			if (*(La.elem + i) == *(Lb.elem + j))
+			if (*(La.elem + i) == *(Lb.elem + j)) {
 				k = 0;
-			break;
+				break;
+			}
 		}
 		if (k) {
-			*Lb.elem = *(La.elem + i);
+			*(Lb.elem + Lb.length) = *(La.elem + i);
 			Lb.length++;
 		}
 	}
 }
-
 
 void Difference(SqList& La, SqList Lb)
 {
 	for (int i = 0; i < La.length;) {
 		int k = 0;
 		for (int j = 0; j < Lb.length; j++) {
-			if (*(La.elem + i) == *(Lb.elem + j))
+			if (*(La.elem + i) == *(Lb.elem + j)) {
 				k = 1;
-			break;
+				break;
+			}
 		}
 		if (k) {
 			ElemType e;
 
-			ListDelete(La, i, e);
+			ListDelete(La, i + 1, e);
 			continue;
 		}
 		i++;
